c7082c2fa094f0a1b46d72184528ec58a5030eed
austin shields <ajshields196@gmail.com>
2021-03-29 02:52:29 -0400

    initial commit

--- counterpoint_practice.rb
+++ counterpoint_practice.rb
@@ -1,20 +1,8 @@
 # example: cantus_firmus = [0, 1, 3, 2, 3, 4, 5, 4, 2, 1, 0]
 # w/ half-steps accounted: [0, 2, 5, 4, 5, 7, 9, 7, 4, 2, 0]
 
-#constant variables
 # valid_intervals = [-12, -7, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 7, 8, 12]
 # valid_notes = [-12, -10, -8, -7, -5, -3, -1, 0, 2, 4, 5, 7, 9, 11, 12, 14, 16]
-
-#set up valid movements
-# valid_movements = {}
-# valid_notes.each do |note|
-#   valid_movements[note] = []
-#   valid_intervals.each do |interval|
-#     if valid_notes.include?(note + interval)
-#       valid_movements[note] << interval
-#     end
-#   end
-# end
 
 #rules:
 
@@ -43,67 +31,220 @@
 # - about 8-16 notes total
 # - high note should be toward middle of line
 
-class CantusFirmus
-
+class CantusFirmusScore
   attr_accessor :original_valid_movements, :notes, :length, :current_available_movements, :current_note_position
 
   def initialize
-    @length = 7
+    @length = 8
     @notes = []
     @length.times do
       @notes << 0
     end
+    @iterations = 1
 
-    @valid_intervals = [-2, -1, 1, 2]
-    @valid_notes = [-12, -10, -8, -7, -5, -3, -1, 0, 2, 4, 5, 7, 9, 11, 12, 14, 16] 
-    @original_valid_movements = {-12=>[2], -10=>[-2, 2], -8=>[-2, 1], -7=>[-1, 2], -5=>[-2, 2], -3=>[-2, 2], -1=>[1], 0=>[-1, 2], 2=>[-2, 2], 4=>[-2, 1], 5=>[-1, 2], 7=>[-2, 2], 9=>[-2, 2], 11=>[1], 12=>[-1, 2], 14=>[-2, 2], 16=>[-2]}
     @current_note_position = 0
     @current_available_movements = []
+    @mode = "ionian"
+    @possible = true
   end
 
-  def determine_all_available_movements
-    note_position = 0
-    (@length - 2).times do
-      @current_available_movements[note_position] = @original_valid_movements[@notes[note_position-1]]
-      note_position += 1
+  def determine_original_valid_movements
+    if @mode == "ionian"
+      @original_valid_movements = {
+        -12=>{:steps=>[2], :leaps=>[4, 5, 7, 12]}, 
+        -10=>{:steps=>[-2, 2], :leaps=>[3, 5, 7, 12]}, 
+        -8=>{:steps=>[-2, 1], :leaps=>[-4, 3, 5, 7, 8, 12]}, 
+        -7=>{:steps=>[-1, 2], :leaps=>[-5, -3, 4, 7, 12]}, 
+        -5=>{:steps=>[-2, 2], :leaps=>[-7, -5, -3, 4, 5, 7, 12]}, 
+        -3=>{:steps=>[-2, 2], :leaps=>[-7, -5, -4, 3, 5, 7, 8, 12]}, 
+        -1=>{:steps=>[1], :leaps=>[]}, 
+        0=>{:steps=>[-1, 2], :leaps=>[-12, -7, -5, -3, 4, 5, 7, 12]}, 
+        2=>{:steps=>[-2, 2], :leaps=>[-12, -7, -5, -3, 3, 5, 7, 12]}, 
+        4=>{:steps=>[-2, 1], :leaps=>[-12, -7, -5, -4, 3, 5, 7, 8, 12]}, 
+        5=>{:steps=>[-1, 2], :leaps=>[-12, -5, -3, 4, 7]}, 
+        7=>{:steps=>[-2, 2], :leaps=>[-12, -7, -5, -3, 4, 5, 7]}, 
+        9=>{:steps=>[-2, 2], :leaps=>[-12, -7, -5, -4, 3, 5, 7]}, 
+        11=>{:steps=>[1], :leaps=>[]}, 
+        12=>{:steps=>[-1, 2], :leaps=>[-12, -7, -5, -3, 4]}, 
+        14=>{:steps=>[-2, 2], :leaps=>[-12, -7, -5, -3]}, 
+        16=>{:steps=>[-2], :leaps=>[-12, -7, -5, -4]}
+      }
     end
   end
 
   def determine_current_available_movements
-    @current_available_movements[@current_note_position] = @original_valid_movements[@notes[@current_note_position]]
+    @current_available_movements[@current_note_position] = @original_valid_movements[@notes[@current_note_position]].dup
   end
 
   def execute_movement
-    p @current_available_movements
-    p @current_note_position
-    executed_movement = @current_available_movements[@current_note_position].sample
-    @current_available_movements[@current_note_position] = @current_available_movements[@current_note_position] - [executed_movement]
-    p executed_movement
-    @notes[@current_note_position+1] = @notes[@current_note_position] + executed_movement
-    p @notes
+    if @current_available_movements[@current_note_position][:steps][0] && @current_available_movements[@current_note_position][:leaps][0]
+      if rand(1..100) > 25
+        step
+      else
+        leap
+      end
+    elsif @current_available_movements[@current_note_position][:steps][0]
+      step
+    else
+      leap
+    end
+
+    @notes[@current_note_position+1] = @notes[@current_note_position] + @executed_movement
   end
-  
+
+  def step
+    @executed_movement = @current_available_movements[@current_note_position][:steps].sample
+    @current_available_movements[@current_note_position][:steps] = @current_available_movements[@current_note_position][:steps] - [@executed_movement]
+  end
+
+  def leap
+    @executed_movement = @current_available_movements[@current_note_position][:leaps].sample
+    @current_available_movements[@current_note_position][:leaps] = @current_available_movements[@current_note_position][:leaps] - [@executed_movement]
+  end
+
   def cascade_movement
     while @current_note_position < ( @length - 2 )
       determine_current_available_movements
       execute_movement
       @current_note_position += 1
-      p @notes
+      #p @notes
     end
   end
 
   def available_movement_check
-    if @current_available_movements[@current_note_position][0]
-      p "movements available"
+    if @current_available_movements[@current_note_position][:leaps][0] || @current_available_movements[@current_note_position][:steps][0]
       return true
     else
-      p "no movements available"
       return false
     end
   end
 
-  def penultimate_check
-    if @notes[-2] == 2 || @notes[-2] == -1
+  def iterate
+    @current_note_position -= 1
+    while available_movement_check == false
+      @current_note_position -= 1
+      if @current_note_position < 0
+        p "no possible note combination"
+        @possible = false
+        break
+      end
+    end
+    if @possible == true
+      execute_movement
+      @current_note_position += 1
+      cascade_movement
+      @iterations += 1
+    end
+  end
+
+  def build_cantus_firmus
+    determine_original_valid_movements
+    cascade_movement
+    while CantusFirmusValidator.valid?(@notes) == false && @possible == true
+      iterate
+    end
+    if @possible == true
+      p @notes
+      p "#{@iterations} iterations"
+      CantusFirmusValidatorWithPrintStatements.valid?(@notes)
+    end
+  end
+
+  def build_a_lot
+    1000.times do
+      @length = 8
+      @notes = []
+      @length.times do
+        @notes << 0
+      end
+      @iterations = 1
+
+      @current_note_position = 0
+      @current_available_movements = []
+
+      build_cantus_firmus
+    end
+  end
+end
+
+
+
+class CantusFirmusValidator
+  def self.valid?(notes)
+    @notes = notes
+    return self.penultimate_check && self.range_check && self.climax_check && leap_percentage_check
+  end
+
+  def self.penultimate_check
+    if @notes[-2] > -3 && @notes[-2] < 3 && @notes[-2] != 0
+      return true
+    else
+      return false
+    end
+  end
+
+  def self.climax_check
+    highest_note = (@notes[0] + 2) #starting value assures climax is at least a third above starting pitch
+    highest_note_index = 0
+    climax_presence = false
+
+    i = 0
+    while i < @notes.length
+      if @notes[i] > highest_note
+        highest_note = @notes[i]
+        p
+        if (i / @notes.length.to_f) >= 0.25 && (i / @notes.length.to_f) <= 0.75 #checks if climax is toward middle
+          climax_presence = true
+        else
+          climax_presence = false
+        end
+      elsif @notes[i] == highest_note
+        climax_presence = false
+      end 
+      i += 1
+    end
+
+    if highest_note == 11     #checks for leading tone
+      climax_presence = false
+    end
+
+    return climax_presence
+  end
+
+  def self.range_check
+    if (@notes.max - @notes.min) < 13
+      return true
+    else
+      return false
+    end
+  end
+  
+  def self.leap_percentage_check
+    leaps = 0
+    i = 0
+    while i < (@notes.length - 1)
+      if ((@notes[i+1] - @notes[i]).abs()) >= 3
+        leaps += 1
+      end
+      i += 1
+    end
+
+    if leaps.to_f / @notes.length.to_f < 0.33
+      return true
+    else
+      return false
+    end
+  end
+end
+
+class CantusFirmusValidatorWithPrintStatements
+  def self.valid?(notes)
+    @notes = notes
+    return self.penultimate_check && self.climax_check && self.range_check && leap_percentage_check
+  end
+
+  def self.penultimate_check
+    if @notes[-2] > -3 && @notes[-2] < 3 && @notes[-2] != 0
       p "good penultimate note"
       return true
     else
@@ -112,35 +253,65 @@ class CantusFirmus
     end
   end
 
-  def penultimate_fix
-    while penultimate_check == false
-      @current_note_position -= 1
-      while available_movement_check == false
-        @current_note_position -= 1
-      end
-      execute_movement
-      @current_note_position += 1
-      cascade_movement
+  def self.climax_check
+    highest_note = (@notes[0] + 2) #starting value assures climax is at least a third above starting pitch
+    highest_note_index = 0
+    climax_presence = false
+
+    i = 0
+    while i < @notes.length
+      if @notes[i] > highest_note
+        highest_note = @notes[i]
+        p
+        if (i / @notes.length.to_f) >= 0.25 && (i / @notes.length.to_f) <= 0.75 #checks if climax is toward middle
+          climax_presence = true
+        else
+          climax_presence = false
+        end
+      elsif @notes[i] == highest_note
+        climax_presence = false
+      end 
+      i += 1
+    end
+
+    if highest_note == 11     #checks for leading tone
+      climax_presence = false
+    end
+
+    p "climax presence is #{climax_presence}"
+    p "climax is #{highest_note}"
+    return climax_presence
+  end
+
+  def self.range_check
+    p "range is #{@notes.max - @notes.min} half-steps"
+    if (@notes.max - @notes.min) < 13
+      return true
+    else
+      return false
     end
   end
-
-  def build_cantus_firmus
-    cascade_movement
-    penultimate_fix
-  end
-
-  def build_endlessly # (for testing)
-    while true
-      @current_note_position = 0
-      @notes = []
-      @length.times do
-        @notes << 0
+  
+  def self.leap_percentage_check
+    leaps = 0
+    i = 0
+    while i < (@notes.length - 1)
+      if ((@notes[i+1] - @notes[i]).abs()) >= 3
+        leaps += 1
       end
-      build_cantus_firmus
+      i += 1
+    end
+
+    p "there are #{leaps} leaps"
+    if leaps.to_f / @notes.length.to_f < 0.33
+      return true
+    else
+      return false
     end
   end
-
 end
 
-cantus_firmus = CantusFirmus.new
-cantus_firmus.build_endlessly
+cantus_firmus = CantusFirmusScore.new
+cantus_firmus.build_cantus_firmus
+
+#March 25 - today I separated the cantus firmus logic into two separate classes (one for the score info, one for validation checks). I created a second version of the validation class so I could have one with print checks (for my sanity) and one without (so the code could actually run). I then added validations for the climax, range, and leap percentage. When I stopped I was working on fixing the climax check in order to only allow a climax toward the center of the cantus firmus (line 165). I am struggling because I've realized, based on my current code, that leaps will always trend toward the beginning.``
